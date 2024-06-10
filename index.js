72b2f566f19d4eed64231991d1a1f077de545633
jaykalathiya123 <kalathiyajay020>
2024-06-10 14:29:24 +0530

    js-card

---
+++ index.js
@@ -0,0 +1,445 @@
+// let student = [{
+//         fName : "John",
+//         lName : "Doe",
+//         address : "surat,gujeat",
+//         age: 20,
+//         ponum:9714689976,
+//         grid:9642,
+//     },{
+//     
+//     },{
+//         fName : "prem",
+//         lName : "char",
+//         address : "surat,gujeat",
+//         age: 18,
+//         ponum:1223586525,
+//         grid:2385,
+//     }
+// ]
+
+// student.forEach((ele)=>{
+//     console.log("ele",ele);
+// })
+// document.getElementById("John").innerHTML = "Name : " + student.fName;
+// document.getElementById("Johnl").innerHTML = "last Name : " + student.lName;
+// document.getElementById("Johnage").innerHTML = "last age: " + student.age;
+// document.getElementById("Johnadd").innerHTML = "address: " + student.address;
+// document.getElementById("Johnno").innerHTML = "contect.number: " + student.ponum;
+// document.getElementById("Johngrid").innerHTML = "GRID: " + student.grid;
+
+// let student2 = {
+//     fName : "rajesh",
+//     lName : "patel",
+//     address : "surat,gujeat",
+//     age: 21,
+//     ponum:13541456,
+//     grid:3254,
+// };
+
+// document.getElementById("rajesh").innerHTML = "Name : " + student2.fName;
+// document.getElementById("rajeshl").innerHTML = "last Name : " + student2.lName;
+// document.getElementById("rajeshage").innerHTML = "last age: " + student2.age;
+// document.getElementById("rajeshadd").innerHTML = "address: " + student2.address;
+// document.getElementById("rajeshno").innerHTML = "contect.number: " + student2.ponum;
+// document.getElementById("rajeshgrid").innerHTML = "GRID: " + student2.grid;
+
+
+// let student3 = {
+//     fName : "prem",
+//     lName : "char",
+//     address : "surat,gujeat",
+//     age: 18,
+//     ponum:1223586525,
+//     grid:2385,
+// };
+
+// document.getElementById("prem").innerHTML = "Name : " + student3.fName;
+// document.getElementById("preml").innerHTML = "last Name : " + student3.lName;
+// document.getElementById("premage").innerHTML = "last age: " + student3.age;
+// document.getElementById("premadd").innerHTML = "address: " + student3.address;
+// document.getElementById("premno").innerHTML = "contect.number: " + student3.ponum;
+// document.getElementById("premgrid").innerHTML = "GRID: " + student3.grid;
+
+// let student4 = {
+//     
+// };
+
+// document.getElementById("rakesha").innerHTML = "Name : " + student4.fName;
+// document.getElementById("rakeshal").innerHTML = "last Name : " + student4.lName;
+// document.getElementById("rakeshaage").innerHTML = "last age: " + student4.age;
+// document.getElementById("rakeshaadd").innerHTML = "address: " + student4.address;
+// document.getElementById("rakeshano").innerHTML = "contect.number: " + student4.ponum;
+// document.getElementById("rakeshagrid").innerHTML = "GRID: " + student4.grid;
+
+// let student5 = {
+//     fName : "jay",
+//     lName : "kalathiya",
+//     address : "surat,gujeat",
+//     age: 19,
+//     ponum:7048404846,
+//     grid:6625,
+// };
+
+// document.getElementById("jay").innerHTML = "Name : " + student5.fName;
+// document.getElementById("jayl").innerHTML = "last Name : " + student5.lName;
+// document.getElementById("jayage").innerHTML = "last age: " + student5.age;
+// document.getElementById("jayadd").innerHTML = "address: " + student5.address;
+// document.getElementById("jayno").innerHTML = "contect.number: " + student5.ponum;
+// document.getElementById("jaygrid").innerHTML = "GRID: " + student5.grid;
+
+
+// let student6 = {
+//     fName : "raj",
+//     lName : "dubey",
+//     address : "surat,gujeat",
+//     age: 19,
+//     ponum:122365456,
+//     grid:2262,
+// };
+
+// document.getElementById("raj").innerHTML = "Name : " + student6.fName;
+// document.getElementById("rajl").innerHTML = "last Name : " + student6.lName;
+// document.getElementById("rajage").innerHTML = "last age: " + student6.age;
+// document.getElementById("rajadd").innerHTML = "address: " + student6.address;
+// document.getElementById("rajno").innerHTML = "contect.number: " + student6.ponum;
+// document.getElementById("rajgrid").innerHTML = "GRID: " + student6.grid;
+
+// let student7 = {
+//     fName : "ravi",
+//     lName : "patel",
+//     address : "surat,gujeat",
+//     age: 19,
+//     ponum:254585326,
+//     grid:2582,
+// };
+
+// document.getElementById("ravi").innerHTML = "Name : " + student7.fName;
+// document.getElementById("ravil").innerHTML = "last Name : " + student7.lName;
+// document.getElementById("raviage").innerHTML = "last age: " + student7.age;
+// document.getElementById("raviadd").innerHTML = "address: " + student7.address;
+// document.getElementById("ravino").innerHTML = "contect.number: " + student7.ponum;
+// document.getElementById("ravigrid").innerHTML = "GRID: " + student7.grid;
+
+// let student8= {
+//   
+// };
+
+// document.getElementById("anupam").innerHTML = "Name : " + student8.fName;
+// document.getElementById("anupaml").innerHTML = "last Name : " + student8.lName;
+// document.getElementById("anupamage").innerHTML = "last age: " + student8.age;
+// document.getElementById("anupamadd").innerHTML = "address: " + student8.address;
+// document.getElementById("anupamno").innerHTML = "contect.num: " + student8.ponum;
+// document.getElementById("anupamgrid").innerHTML = "GRID: " + student8.grid;
+
+// let student9= {
+//     fName : "jack",
+//     lName : "sharma",
+//     address : "surat,gujeat",
+//     age: 17,
+//     ponum:2536147878,
+//     grid:25812,
+// };
+
+// document.getElementById("jack").innerHTML = "Name : " + student9.fName;
+// document.getElementById("jackl").innerHTML = "last Name : " + student9.lName;
+// document.getElementById("jackage").innerHTML = "last age: " + student9.age;
+// document.getElementById("jackadd").innerHTML = "address: " + student9.address;
+// document.getElementById("jackno").innerHTML = "contect.num: " + student9.ponum;
+// document.getElementById("jackgrid").innerHTML = "GRID: " + student9.grid;
+
+// let student10= {
+//     fName : "harsha",
+//     lName : "patel",
+//     address : "surat,gujeat",
+//     age: 21,
+//     ponum:25147878,
+//     grid:2582,
+// };
+
+// document.getElementById("harsha").innerHTML = "Name : " + student10.fName;
+// document.getElementById("harshal").innerHTML = "last Name : " + student10.lName;
+// document.getElementById("harshaage").innerHTML = "last age: " + student10.age;
+// document.getElementById("harshaadd").innerHTML = "address: " + student10.address;
+// document.getElementById("harshano").innerHTML = "contect.num: " + student10.ponum;
+// document.getElementById("harshagrid").innerHTML = "GRID: " + student9.grid;
+
+
+// let student11= {
+
+// };
+
+// document.getElementById("ronak").innerHTML = "Name : " + student11.fName;
+// document.getElementById("ronakl").innerHTML = "last Name : " + student11.lName;
+// document.getElementById("ronakage").innerHTML = "last age: " + student11.age;
+// document.getElementById("ronakadd").innerHTML = "address: " + student11.address;
+// document.getElementById("ronakno").innerHTML = "contect.num: " + student11.ponum;
+// document.getElementById("ronakgrid").innerHTML = "GRID: " + student11.grid;
+
+// let student12= {
+
+// };
+
+// document.getElementById("rahul").innerHTML = "Name : " + student12.fName;
+// document.getElementById("rahull").innerHTML = "last Name : " + student12.lName;
+// document.getElementById("rahulage").innerHTML = "last age: " + student12.age;
+// document.getElementById("rahuladd").innerHTML = "address: " + student12.address;
+// document.getElementById("rahulno").innerHTML = "contect.num: " + student12.ponum;
+// document.getElementById("rahulgrid").innerHTML = "GRID: " + student12.grid;
+
+// let student13= {
+//     fName : "ankur",
+//     lName : "changani",
+//     address : "surat,gujeat",
+//     age: 17,
+//     ponum:8596741023,
+//     grid:985,
+// };
+
+// document.getElementById("ankur").innerHTML = "Name : " + student13.fName;
+// document.getElementById("ankurl").innerHTML = "last Name : " + student13.lName;
+// document.getElementById("ankurage").innerHTML = "last age: " + student13.age;
+// document.getElementById("ankuradd").innerHTML = "address: " + student13.address;
+// document.getElementById("ankurno").innerHTML = "contect.num: " + student13.ponum;
+// document.getElementById("ankurgrid").innerHTML = "GRID: " + student13.grid;
+
+// let student14= 
+
+// document.getElementById("anuj").innerHTML = "Name : " + student14.fName;
+// document.getElementById("anujl").innerHTML = "last Name : " + student14.lName;
+// document.getElementById("anujage").innerHTML = "last age: " + student14.age;
+// document.getElementById("anujadd").innerHTML = "address: " + student14.address;
+// document.getElementById("anujno").innerHTML = "contect.num: " + student14.ponum;
+// document.getElementById("anujgrid").innerHTML = "GRID: " + student14.grid;
+
+// let student15= {
+// 
+// };
+
+// document.getElementById("parth").innerHTML = "Name : " + student15.fName;
+// document.getElementById("parthl").innerHTML = "last Name : " + student15.lName;
+// document.getElementById("parthage").innerHTML = "last age: " + student15.age;
+// document.getElementById("parthadd").innerHTML = "address: " + student15.address;
+// document.getElementById("parthno").innerHTML = "contect.num: " + student15.ponum;
+// document.getElementById("parthgrid").innerHTML = "GRID: " + student15.grid;
+const StudantData = document.getElementById("StudantData");
+
+
+let studant = [{
+    fName: "John",
+    lName: "Doe",
+    address: "surat,gujeat",
+    age: 20,
+    ponum: 9714689976,
+    grid: 9642,
+    hobby: ["music", "cooking"],
+}, {
+    fName: "tejash",
+    lName: "shinde",
+    address: "surat,gujeat",
+    age: 21,
+    ponum: 523543232,
+    grid: 9642,
+    hobby: ["games", "play", "Dance"],
+}, {
+    fName: "jayant",
+    lName: "patodiya",
+    address: "surat,gujeat",
+    age: 18,
+    ponum: 87985485,
+    grid: 8598,
+    hobby: ["games", "play"],
+}, {
+    fName: "Aauj",
+    lName: "Rajput",
+    address: "surat,gujeat",
+    age: 18,
+    ponum: 523543232,
+    grid: 8598,
+    hobby: ["cooking", "play"],
+}, {
+    fName: "rajesh",
+    lName: "patel",
+    address: "surat,gujeat",
+    age: 21,
+    ponum: 13541456,
+    grid: 3254,
+    hobby: ["cooking", "play"],
+}, {
+    fName: "Aaus",
+    lName: "Rajput",
+    address: "surat,gujeat",
+    age: 18,
+    ponum: 523543232,
+    grid: 8598,
+    hobby: ["cooking", "play"],
+},
+{
+    fName: "Aaus",
+    lName: "Rajput",
+    address: "surat,gujeat",
+    age: 18,
+    ponum: 523543232,
+    grid: 8598,
+    hobby: ["cooking", "play"],
+},
+{
+    fName: "Aaus",
+    lName: "Rajput",
+    address: "surat,gujeat",
+    age: 21,
+    ponum: 523543232,
+    grid: 8598,
+    hobby: ["cooking", "play"],
+},
+{
+    fName: "Aaus",
+    lName: "Rajput",
+    address: "surat,gujeat",
+    age: 20,
+    ponum: 523543232,
+    grid: 8598,
+    hobby: ["cooking", "play"],
+},
+{
+    fName: "Aaus",
+    lName: "Rajput",
+    address: "surat,gujeat",
+    age: 18,
+    ponum: 523543232,
+    grid: 8598,
+    hobby: ["cooking", "play"],
+}, {
+    fName: "anuj",
+    lName: "shig",
+    address: "surat,gujeat",
+    age: 17,
+    ponum: 8596741023,
+    grid: 985,
+    hobby: ["play", "reding"],
+}, {
+    fName: "parth",
+    lName: "singaniya",
+    address: "surat,gujeat",
+    age: 21,
+    ponum: 8596741285,
+    grid: 8578,
+    hobby: ["cooking", "play"],
+},
+{
+    fName: "rahul",
+    lName: "sharma",
+    address: "surat,gujeat",
+    age: 20,
+    ponum: 5263417485,
+    grid: 8956,
+    hobby: ["games", "play", "Dance"],
+},
+{
+    fName: "rakesha",
+    lName: "patel",
+    address: "surat,gujeat",
+    age: 18,
+    ponum: 2514362514,
+    grid: 2578,
+    hobby: ["music", "cooking"],
+},
+{
+    fName: "ronak",
+    lName: "chavda",
+    address: "surat,gujeat",
+    age: 19,
+    ponum: 7485964152,
+    grid: 5289,
+    hobby: ["games", "play"],
+
+},
+{
+    fName: "ronak",
+    lName: "chavda",
+    address: "surat,gujeat",
+    age: 19,
+    ponum: 7485964152,
+    grid: 5289,
+    hobby: ["games", "play"],
+
+},
+{
+    fName: "rajesh",
+    lName: "patel",
+    address: "surat,gujeat",
+    age: 21,
+    ponum: 13541456,
+    grid: 3254,
+    hobby: ["games",  "Dance"],
+},
+{
+    fName : "anupam",
+        lName : "kher",
+        address : "surat,gujeat",
+        age: 17,
+        ponum:2536147878,
+        grid:25812,
+        hobby: ["Dance"],
+},
+{
+    fName : "raj",
+        lName : "dubey",
+        address : "surat,gujeat",
+        age: 19,
+        ponum:122365456,
+        grid:2262,
+        hobby: ["games",  "Dance"],
+},
+{
+    fName: "Shubham",
+    lName: "Thakur",
+    age: 21,
+    gmail: "subT78@gmail.com",
+    ponum: 523543232,
+    address: "surat,gujeat",
+    grid: 8598,
+    hobby: ["play"],
+}];
+
+
+// StudantData.innerHTML += (`<div class="col-4"><div class="card p-3"><h3>Name : ${studant[0].name}</h3><h3>lname : ${studant[0].lname}</h3></div></div> `);
+// StudantData1.innerHTML += (`<div class="col-4"><div class="card p-3">Name : ${studant[1].name}</div></div> `);
+
+// for(let i = 0; i < studant.length; i++){
+
+//     StudantData.innerHTML += (`<div class="col-6"><div class="card p-3"><h3>Name : ${studant[i].name}</h3><h3>lname : ${studant[i].lname}</h3><h3>age : ${studant[i].age}</h3><h3>gender : ${studant[i].gender}</h3><h3>grId : ${studant[i].grId}</h3><h3>gmail : ${studant[i].gmail}</h3><h3>contact : ${studant[i].contact}</h3><h3>hobby : ${studant[i].hobby}</h3><h3>address : ${studant[i].address.place}</h3><h3>city : ${studant[i].city}</h3><h3>country : ${studant[i].country}</h3></div></div>`);
+
+//     console.log(studant[i].lname);
+// };
+
+studant.forEach((ele) => {
+    console.log("ele", ele);
+    StudantData.innerHTML += (`
+                                <div class="container">
+                                        <h1>
+                                            <div class="card mt">
+                                                <h1>student id</h1>
+                                                <div class="row ">
+                                                    <h3  class="ml-30"> Name:${ele.fName}</h3>
+                                                    <h3 id="Johnl">last Name: ${ele.lName}</h3>
+                                                </div>
+                                            <div class="row mt">
+                                                <h3  class="ml-30">Age: ${ele.age}</h3>
+                                                <h3  class=""> Hoddy:${ele.hobby}</h3>
+                                            </div>
+                                            <div class="row mt">
+                               
+                                                <h3 class=""> Address:  ${ele.address}</h3>
+                                            </div>
+                                            <div class="row">
+                                            <h3 class="ml-30">GRID: ${ele.grid}</h3>
+                                            <h3>Contect.no: ${ele.ponum}</h3>
+
+                                            </div>
+                                            </div>
+                                        </div>
+                                
+                                
+                                `);
+
+})
